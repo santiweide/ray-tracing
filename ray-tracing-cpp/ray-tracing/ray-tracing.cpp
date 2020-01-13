9706c6da1693ef08f7f1c3ac108b6d20a4668555
santiweide <ggy_778@mail.dlut.edu.cn>
2020-01-13 18:58:40 +0800

    finish camera

--- ray-tracing-cpp/ray-tracing/ray-tracing.cpp
+++ ray-tracing-cpp/ray-tracing/ray-tracing.cpp
@@ -21,17 +21,9 @@ int main()
 	int ny = 100;
 	int ns = 100;
 	FILE* stream;
-	freopen_s(&stream, "2.ppm", "w", stdout);
+	freopen_s(&stream, "3.ppm", "w", stdout);
 	printf("P3\n%d %d\n255\n", nx, ny);
-	vec3 lower_left_corner;
-	vec3 origin;
-	vec3 horizontal;
-	vec3 vertical;
-	lower_left_corner = vec3(-2.0, -1.0, -1.0);
-	horizontal = vec3(4.0, 0.0, 0.0);
-	vertical = vec3(0.0, 2.0, 0.0);
-	origin = vec3(0, 0, 0);
-	
+	camera cam;
 	hitable* list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
@@ -39,10 +31,14 @@ int main()
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			vec3 col(0, 0, 0);
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			ray r = ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);	
-			col = color(r, world);
+			for (int s = 0; s < ns ; s++)
+			{
+				float u = float(i + rand() / double(RAND_MAX)) / float(nx);
+				float v = float(j + rand() / double(RAND_MAX)) / float(ny);
+				ray r = cam.get_ray(u, v);
+				col = col + color(r, world);
+			}
+			col = col / float(ns);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
@@ -52,39 +48,3 @@ int main()
 	fclose(stdout);
 	return 0;
 }
-
-int main_c()
-{
-	int nx = 200;
-	int ny = 100;
-	int ns = 100;
-	FILE* stream;
-	freopen_s(&stream,"2.ppm","w",stdout);
-	printf("P3\n%d %d\n255\n",nx,ny);
-	hitable* list[2];
-	list[0] = new sphere(vec3(0,0,-1),0.5);
-	list[1] = new sphere(vec3(0, -100.5, -1), 100);
-	hitable* world = new hitable_list(list, 2);
-	camera cam;
-	for (int j = ny - 1; j >= 0; j--) {
-		for (int i = 0; i < nx; i++) {
-			vec3 col(0, 0, 0);
-			for (int s = 0; s < ns; s++)
-			{
-				float u = float(i + rand()) / float(nx);
-				float v = float(j + rand()) / float(ny);
-				ray r = cam.get_ray(u,v);
-				vec3 p = r.point_at_parameter(2.0);
-				col = col + color(r, world);
-			}
-			col = col / float(ns);
-			int ir = int(255.99 * col[0]);
-			int ig = int(255.99 * col[1]);
-			int ib = int(255.99 * col[2]);
-			printf("%d %d %d\n",ir,ig,ib);
-		}
-	}
-	fclose(stdout);
-	return 0;
-}
-
